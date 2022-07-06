2dd78771e0ecf02d4f9db0a207c07be46d4c329f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-06 20:59:04 +0100

    added models and category seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ProductsSeeder::class
+            ProductsSeeder::class,
+            CategoriesSeeder::class
         ]);
     }
 }
