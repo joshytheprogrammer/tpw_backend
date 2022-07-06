685b9a0104f8a435e4b4e575d48120891f38676e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-06 17:38:02 +0100

    created table migrations

---
+++ database/migrations/2022_07_06_162244_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unique('customer_id');
+            $table->json('products');
+            $table->integer('amount');
+            $table->string('status');
+            $table->string('payment_mode');
+            $table->string('fulfillment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
