926cd6825200daa343552e11a38fc0d0fbfa9e2c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-08 20:21:22 +0100

    made changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Resources\Json\JsonResource as ProductsResource;
 
 class HomeController extends Controller
 {
-    //
     public function recommended() {
         $products = Product::select(['_id', '_slug', 'thumbnail', 'name', 'price'])->orderBy('created_at', 'desc')->get();
 
