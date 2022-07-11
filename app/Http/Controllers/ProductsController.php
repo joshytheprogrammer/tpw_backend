98aa83ede4f5fcd4df86b0e5761b73c5c3832b2c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 17:55:21 +0100

    created shop route

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Http\Resources\Json\JsonResource as ProductsResource;
+
+class ProductsController extends Controller
+{
+    public function recommended() {
+        $products = Product::select(['_id', '_slug', 'thumbnail', 'name', 'price'])->orderBy('created_at', 'desc')->get();
+
+        return new ProductsResource($products);
+    }
+}
