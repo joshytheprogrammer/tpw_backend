0775ee170f3eaf427760bdaa47c6c2f78cfc5c39
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-07-11 17:34:42 +0100

    make slug more mininful

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -18,16 +18,16 @@ class ProductsSeeder extends Seeder
     {
         DB::table('products')->insert([
             [
-                '_id' => substr(bin2hex(random_bytes(32)), 0, 8),
-                '_slug' => 'search-slug-'.rand(0, 20000),
+                '_id' => 'gwer435t',
+                '_slug' => '10-inch-double-layer-whipped-cream-cake-gwer435t',
                 'thumbnail' => 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1650226544/tec/products/edited_image_with_label_vrbcdx.png', 
                 'name' => '10 inch Double Layer Whipped Cream Cake',
                 'price' => json_encode(["lowest" => "20500", "highest" => "27500"]),
                 'created_at' => Carbon::now()
             ],
             [
-                '_id' => substr(bin2hex(random_bytes(32)), 0, 8),
-                '_slug' => 'search-slug-'.rand(0, 20000),
+                '_id' => 'flr34tnb',
+                '_slug' => 'single-layer-8-inch-butter-cream-cake-flr34tnb',
                 'thumbnail' => 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1649158427/tec/products/1D22D433-FA9A-43A8-B46B-7DB3715FE0C6_pv1wsw.jpg', 
                 'name' => 'Single Layer 8 inch Butter Cream Cake',
                 'price' => json_encode(["lowest" => "6000", "highest" => "14000"]),
@@ -35,7 +35,7 @@ class ProductsSeeder extends Seeder
             ],
             [
                 '_id' => substr(bin2hex(random_bytes(32)), 0, 8),
-                '_slug' => 'search-slug-'.rand(0, 20000),
+                '_slug' => 'single-layer-10-inch-butter-cream-cake',
                 'thumbnail' => 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1649163828/tec/products/IMG_6212_leid3k.jpg', 
                 'name' => 'Single Layer 10 inch Butter Cream Cake',
                 'price' => json_encode(["lowest" => "25000", "highest" => "30000"]),
@@ -43,7 +43,7 @@ class ProductsSeeder extends Seeder
             ],
             [
                 '_id' => substr(bin2hex(random_bytes(32)), 0, 8),
-                '_slug' => 'search-slug-'.rand(0, 20000),
+                '_slug' => 'butter-cream-cake-with-chocolate-drip',
                 'thumbnail' => 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1649158052/tec/products/signal-2022-04-05-122625_001_zrzlp3.jpg', 
                 'name' => 'Butter Cream Cake With Chocolate Drip',
                 'price' => json_encode(["lowest" => "12000", "highest" => "19000"]),
@@ -51,7 +51,7 @@ class ProductsSeeder extends Seeder
             ],
             [
                 '_id' => substr(bin2hex(random_bytes(32)), 0, 8),
-                '_slug' => 'search-slug-'.rand(0, 20000),
+                '_slug' => 'simple-two-tier-wedding-cake',
                 'thumbnail' => 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1650224468/tec/products/enhanced_image_1_nxnoxo.jpg', 
                 'name' => 'Simple Two Tier Wedding Cake',
                 'price' => json_encode(["lowest" => "50000", "highest" => "70000"]),
