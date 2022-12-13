131f4d4da6523c69f54927743d682a728bea66ab
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-13 09:13:19 +0100

    made changes

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Http\Resources\Json\JsonResource as ProductsResource;
 
 class SearchController extends Controller
 {
     public function search(Request $request) {
         $query = $request["query"];
 
-        
+        $products = $this->searchProductColumns($query);
+
+        return $products;
+    }
+
+    protected function rankSearch() {
+
+    }
+
+    protected function searchProductColumns($query) {
+        return Product::select(['_id', '_slug', 'thumbnail', 'name', 'price'])
+        ->where('_slug', 'like', '%'.$query.'%')
+        ->orWhere('_id', 'like', '%'.$query.'%')
+        ->orWhere('thumbnail', 'like', '%'.$query.'%')
+        ->orWhere('name', 'like', '%'.$query.'%')
+        ->get();
     }
 }
